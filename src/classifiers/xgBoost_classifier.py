6289237753583da785e53fd426f31a0e0d1ccfb9
Davis <davislyu@gmail.com>
2024-07-26 18:46:30 +0300

    Added Confusion Matrix

--- src/classifiers/xgBoost_classifier.py
+++ src/classifiers/xgBoost_classifier.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as  pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, f1_score, confusion_matrix
 import xgboost as xgb
