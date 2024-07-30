759527a8f87c9a76a6d6c668847a7083825db799
Davis <davislyu@gmail.com>
2024-07-30 18:43:34 +0300

    Updated XGBoost_With_Hyperparameter.py

--- src/classifiers/XGBoost_With_Hyperparameter.py
+++ src/classifiers/XGBoost_With_Hyperparameter.py
@@ -5,38 +5,27 @@ import xgboost as xgb
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Load the data
 file_path = 'data/processed/final_embeddings.xlsx'
 data = pd.read_excel(file_path)
 
-# Separate features and target
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 
-# Ensure target is integer type
 y = y.astype(int)
 
-# Display first few rows of labels and unique values
-print(f"First few rows of labels to validate:\n{y.head()}")
-print(f"Unique values in the label column:\n{y.unique()}")
 
-# Validate labels
 valid_labels = {1, 2, 3}
 if not set(y.unique()).issubset(valid_labels):
     raise ValueError("Labels should be numeric (1, 2, 3).")
 
-# Adjust labels to start from 0 for compatibility with the XGBoost classifier
 y = y - 1
 
-# Define class names and labels
 label_classes = [0, 1, 2]
 print(f"Encoded labels:\n{y.head()}")
 
-# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 print(f"Shapes of X_train: {X_train.shape}, X_test: {X_test.shape}, y_train: {y_train.shape}, y_test: {y_test.shape}")
 
-# Train the XGBoost classifier with hyperparameters
 xgb_classifier = xgb.XGBClassifier(
     max_depth=5,            # Maximum depth of a tree
     learning_rate=0.1,      # Learning rate (step size shrinkage)
@@ -48,10 +37,8 @@ xgb_classifier = xgb.XGBClassifier(
 )
 xgb_classifier.fit(X_train, y_train)
 
-# Predict on test data
 y_pred = xgb_classifier.predict(X_test)
 
-# Calculate accuracy and F1 scores
 accuracy = xgb_classifier.score(X_test, y_test)
 macro_f1 = f1_score(y_test, y_pred, average='macro')
 micro_f1 = f1_score(y_test, y_pred, average='micro')
@@ -64,14 +51,11 @@ print(f"Micro F1-score: {micro_f1}")
 y_test = y_test + 1
 y_pred = y_pred + 1
 
-# Define target names for classification report and confusion matrix
 target_names = ['Supportive', 'Opposed', 'Neutral']
 
-# Generate classification report
 report = classification_report(y_test, y_pred, target_names=target_names, zero_division=0)
 print(f"Classification Report:\n{report}")
 
-# Generate confusion matrix
 cm = confusion_matrix(y_test, y_pred, labels=[1, 2, 3])
 
 # Plot confusion matrix
